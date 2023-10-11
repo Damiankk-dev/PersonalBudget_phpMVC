4d3ed25afb9d23a5167a8219d1bcfe5136818c43
damiankk <damian.kaczmarzyk.programista@gmail.com>
2023-10-11 19:28:02 +0200

    Fill limit modal with values and show limit in setting index

--- public/js/limit.js
+++ public/js/limit.js
@@ -1,24 +1,65 @@
-let limitButtons = document.querySelectorAll(".expense_limit");
-for (let i = 0; i < limitButtons.length; i++){
-    limitButtons[i].addEventListener("click", () => {
-        showLimitModal();
-    })
+const setLimitValues = async () => {
+    let limitRows = document.querySelectorAll(".category-row");
+    for (let i = 0; i<limitRows.length; i++){
+        let limitButton = limitRows[i].querySelector("button.expense_limit")
+        let expenseId = limitButton.getAttribute("id");
+        expenseId = expenseId.split('_')[2];
+        let limitData = await getLimitForCategory(expenseId);
+        console.log(limitData.category_limit);
+
+        let categoryName = limitRows[i].querySelector("input.category-name").getAttribute("value");
+
+        limitButton.addEventListener("click", async () => {
+            showLimitModal(categoryName, limitData.category_limit);
+        })
+
+        if (limitData.category_limit !== null){
+            let limitValue = limitRows[i].querySelector("span.limit-value");
+            limitValue.textContent = limitData.category_limit;
+            let limitLabel = limitRows[i].querySelector("label.expense_limit");
+            limitLabel.removeAttribute("hidden");
+        }
+
+
+    }
+
 }
 
 let limitModalCheckbox = document.querySelector("#add-limit");
-limitModalCheckbox.addEventListener("click", () => {
+limitModalCheckbox.addEventListener("click", async () => {
     if (limitModalCheckbox.checked === true){
         document.querySelector(".limit-modal-input-number").removeAttribute("disabled");
     } else {
         document.querySelector(".limit-modal-input-number").setAttribute("disabled", "");
     };
 })
-const showLimitModal = () => {
+const showLimitModal = (categoryName, limitValue) => {
     $('#limitModal').on('show.bs.modal', function (e) {
-        var modal = $(this);
-        modal.find(".limit-category-name").text("XD")
+        let modal = document.querySelector("#limitModal");
+        let modalName = modal.querySelector(".limit-category-name");
+        modalName.textContent = categoryName;
+        let valueInput = modal.querySelector(".limit-modal-input-number");
+        valueInput.setAttribute("value", limitValue);
+        let limitModalCheckbox = modal.querySelector("#add-limit");
+        if (limitValue !== null) {
+            limitModalCheckbox.setAttribute("checked", false);
+            modal.querySelector(".limit-modal-input-number").removeAttribute("disabled");
+        }
       });
+
     $('#limitModal').modal('show');
 }
 
 const closeLimitModal = () => $('#limitModal').modal('hide');
+
+const getLimitForCategory = async (categoryId) => {
+    try{
+        const res = await fetch(`../api/limit/${categoryId}`);
+        const data = await res.json();
+        return data;
+    }catch (e){
+        console.log("ERROR", e);
+    }
+}
+
+setLimitValues();
