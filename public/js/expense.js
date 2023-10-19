1e3575974f726729b5ede69f3a49e3a0e800db76
damiankk <damian.kaczmarzyk.programista@gmail.com>
2023-10-19 21:51:09 +0200

    read expense form values by script and simply react for limit value then put value to a limit flash message space

--- public/js/expense.js
+++ public/js/expense.js
@@ -1,1 +1,41 @@
-alert("expense");
+let categorySelect = document.querySelector("#expense_category");
+categorySelect.addEventListener("change", async () => {
+    setLimitValue();
+})
+
+const getLimitForCategory = async (categoryId) => {
+    try{
+        const res = await fetch(`../api/limit/${categoryId}`);
+        const data = await res.json();
+        return data;
+    }catch (e){
+        console.log("ERROR", e);
+    }
+}
+
+
+const setLimitValue = async () => {
+    let categorySelect = document.querySelector("#expense_category");
+    let categoryId = categorySelect.value;
+    let category_limit = await getLimitForCategory(categoryId);
+    console.log(category_limit);
+    let limitMessageSpan = document.querySelector("#limit-alert-limit-value");
+    limitMessageSpan.textContent = category_limit.category_limit;
+}
+
+setLimitValue();
+
+let expenseValueInput = document.querySelector("#expense_amount");
+expenseValueInput.addEventListener("change", async () => {
+    let limitValue = document.querySelector("#limit-alert-limit-value").textContent;
+    if (limitValue != ''){
+        limitValue = parseFloat(limitValue);
+        let expenseValue = parseFloat(expenseValueInput.value);
+        if (expenseValue > limitValue){
+            console.log("LIMIT EXCEDEED!!!");
+        } else {
+            console.log("LIMIT OK :) ");
+        }
+    }
+    console.log(limitValue);
+})
