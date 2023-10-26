22c29ba2f75c90616818a1b5584a5c50aecd995a
damiankk <damian.kaczmarzyk.programista@gmail.com>
2023-10-26 08:09:24 +0200

    Added a proper messages to a limit alerts

--- public/js/expense.js
+++ public/js/expense.js
@@ -3,19 +3,19 @@
 let categorySelect = document.querySelector("#expense_category");
 categorySelect.addEventListener("change", async () => {
     setLimitValue();
-    await setStatusOfExpense();
     await showExpenseStatus();
 })
     //Value input
 let expenseValueInput = document.querySelector("#expense_amount");
 expenseValueInput.addEventListener("change", async () => {
-    await setStatusOfExpense();
+    await showExpenseStatus();
+})
+expenseValueInput.addEventListener("keydown", async () => {
     await showExpenseStatus();
 })
     //Datepicker
 let expenseDate = document.querySelector("#expense_date");
 expenseDate.addEventListener("change", async () => {
-    await setStatusOfExpense();
     await showExpenseStatus();
 })
 
@@ -48,23 +48,19 @@ const getMonthlySumOfExpensesForCategory = async () => {
 //Other funcs
 const setLimitValue = async () => {
     let category_limit = await getLimitForCategory();
-    let limitMessageSpan = document.querySelector("#limit-alert-limit-value");
-    limitMessageSpan.textContent = category_limit.category_limit;
+    let limitMessageSpan = document.querySelector("#limit-value");
+    limitMessageSpan.value = category_limit.category_limit;
 }
 
 setLimitValue();
 
 const setStatusOfExpense = async () => {
     let status = "idle";
-    let limit = document.querySelector("#limit-alert-limit-value").textContent;
+    let limit = document.querySelector("#limit-value").value;
     if (limit == '') limit = null;
     else limit = parseFloat(limit);
-    let monthlyExpenses = 0.0;
-    let jsonMonthlyExpenses = await getMonthlySumOfExpensesForCategory();
-    if (jsonMonthlyExpenses.categorySum != null) monthlyExpenses = parseFloat(jsonMonthlyExpenses.categorySum);
-    let currentExpenseValue = parseFloat(expenseValueInput.value);
-    let sumExpenses = monthlyExpenses + currentExpenseValue;
-    if ((limit != null && sumExpenses > 0.0) || sumExpenses < limit) status = "OK";
+    let sumExpenses = await setSumOfExpenses();
+    if (limit != null && sumExpenses < limit) status = "OK";
     else if (limit != null && sumExpenses >= limit) status = "WARN";
 
     return status;
@@ -74,15 +70,18 @@ const showExpenseStatus = async () => {
     let expenseStatus = await setStatusOfExpense();
     let limitMessageText = document.querySelector("#limit-alert-message");
     let limitMessageField = document.querySelector("#limit-alert");
+    let limitValue = document.querySelector("#limit-value").value;
+    let expenses = document.querySelector("#sum-of-expenses").value;
+    let difference = parseFloat(limitValue - expenses).toFixed(2);
     switch (expenseStatus){
         case 'OK':
-            limitMessageText.textContent = "Jest OK!";
+            limitMessageText.textContent = `Jest OK! W tym miesiącu kwota wydatków: ${expenses} jest poniżej kwoty limitu: ${limitValue}. Do wykorzystania w ramch limitu pozostało: ${difference}`;
             limitMessageField.removeAttribute("hidden");
             limitMessageField.classList.remove("alert-warning");
             limitMessageField.classList.add("alert-info");
         break;
         case 'WARN':
-            limitMessageText.textContent = "No i klops";
+            limitMessageText.textContent = `Uwaga! W tym miesiącu kwota wydatków: ${expenses} przekroczyła kwotę limitu: ${limitValue} o ${-1*difference}`;
             limitMessageField.removeAttribute("hidden");
             limitMessageField.classList.remove("alert-info");
             limitMessageField.classList.add("alert-warning");
@@ -92,4 +91,15 @@ const showExpenseStatus = async () => {
             limitMessageText.textContent = "";
             limitMessageField.setAttribute("hidden","");
     }
+}
+
+const setSumOfExpenses = async () => {
+    let monthlyExpenses = 0.0;
+    let jsonMonthlyExpenses = await getMonthlySumOfExpensesForCategory();
+    if (jsonMonthlyExpenses.categorySum != null) monthlyExpenses = parseFloat(jsonMonthlyExpenses.categorySum);
+    let currentExpenseValue = parseFloat(expenseValueInput.value);
+    let sumExpenses = monthlyExpenses + currentExpenseValue;
+    let sumValue = document.querySelector("#sum-of-expenses");
+    if (sumExpenses ) sumValue.value = sumExpenses;
+    return sumExpenses;
 }
